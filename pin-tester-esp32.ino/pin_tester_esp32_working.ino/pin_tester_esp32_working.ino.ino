79d49ab6bbdd3986c2ee8df0f593d62636d2d494
cojetidotohopico <nikolas.fillo1@gmail.com>
2026-03-29 16:30:38 +0200

    Update pin_tester_esp32_working.ino.ino

--- pin-tester-esp32.ino/pin_tester_esp32_working.ino/pin_tester_esp32_working.ino.ino
+++ pin-tester-esp32.ino/pin_tester_esp32_working.ino/pin_tester_esp32_working.ino.ino
@@ -2,11 +2,11 @@ int testovaciPiny[] = {2, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 
 
 void setup() { 
    Serial.begin(115200); 
-   Serial.println("starting test")
+   Serial.println("starting test");
 
    for (int i= 0; i < sizeof(testovaciPiny) / sizeof(int); i++) { 
      pinMode(testovaciPiny[i], OUTPUT);
-     digitalWrite(testovaciPiny[i], HIGH)
+     digitalWrite(testovaciPiny[i], HIGH);
      Serial.print("ACTIVATING PIN: ");
      Serial.println(testovaciPiny[i]);
    }
