43373fa31413cf71ea663ea1f51af6f70e533bdf
cojetidotohopico <nikolas.fillo1@gmail.com>
2026-03-29 15:39:09 +0200

    Add files via upload
    version 1.0

---
+++ pin-tester-esp32.ino/pin-tester-esp32.ino.ino
@@ -0,0 +1,17 @@
+int testovaciPiny[] = {2, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33};
+
+void setup() { 
+   Serial.begin(115200); 
+   Seraial.println("starting test")
+
+   for (int i= 0; i < sizeof(testovaciPiny) / sizeof(int); i++) { 
+     pinMode(testovaciPiny[i], OUTPUT);
+     digitalWrite(testovaciPiny[i], HIGH)
+     Serial.print("ACTIVATING PIN: ");
+     Serial.println(testovaciPiny[i]);
+   }
+   Serial.println("All pins are under electricity, you can start testing");
+}
+
+void loop() {
+}
